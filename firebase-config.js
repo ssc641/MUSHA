3cc0d02c50fe92ba5b69e43bdb0250dd16201496
Staff Chingwarire <128416812+ssc641@users.noreply.github.com>
2026-05-08 10:31:49 +0200

    Add Firebase configuration and initialize app
    Add Firebase configuration and initialization code.

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,20 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC2tEGjPcpz5bCac-fNLfnvTdlPj76Ix5I",
+  authDomain: "musha-a9afd.firebaseapp.com",
+  projectId: "musha-a9afd",
+  storageBucket: "musha-a9afd.firebasestorage.app",
+  messagingSenderId: "746382138126",
+  appId: "1:746382138126:web:38b5fa27d3436169fdcd9f",
+  measurementId: "G-Y8GJN9K0KN"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// These are the "Shortcuts" we will use in your other scripts
+const db = firebase.firestore();
+const storage = firebase.storage();
+
+console.log("Firebase is connected to Musha-a9afd!");
 
